cbe2c6a92396180b2ef3be7a33268f1395300025
Fidel Thomet <git@ft0.ch>
2025-11-25 11:57:49 +0100

    reenable caching

--- api.js
+++ api.js
@@ -121,7 +121,7 @@ export async function getFilters(force = false) {
 
 export async function getCreators(force = false) {
   const cached = await getCache("creators");
-  // if (cached && !force) return cached;
+  if (cached && !force) return cached;
 
   const items = await getAllItems();
 
@@ -250,7 +250,7 @@ export async function queryItems(
 
   const { queryString, isFiltered, limit } = parseQuery(query);
   const cached = await getCache(`query:${queryString}`);
-  // if (cached) return cached;
+  if (cached) return cached;
 
   const url = `${OMEKA_API}/items?sort_by=created&sort_order=desc&${queryString}`;
   const res = await fetch(url);
